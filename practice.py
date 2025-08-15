3293ac044589ff09921ac536282ed22a1e5fdaf4
prateeekk07 <prateekc53@gmail.com>
2025-08-14 22:43:26 -0500

    added a file

---
+++ practice.py
@@ -0,0 +1,99 @@
+print("Hello World")
+
+# Numeric
+    # integer
+    # float
+    # complex number
+
+# Sequence
+    # string
+    # list
+    # tuple
+    # set
+    # frozenset
+    # range
+    # bytes
+    # bytearray
+
+# Mapping
+    # dictionary
+
+# Boolean
+
+# NoneType
+
+fullName = "Prateek" #string 
+
+phoneNumber = 9856213012 #integer
+
+currency = 65.5 #float
+
+abc = 55+65j #complex number
+
+fruits = ["orange","mango","cherry","banana"] #list
+
+cars = ("xuv","i20","g-wagon","thar") #tuple
+
+r = range(1,11) #range 1 2 3.. 10
+
+biks = {"pulsar","bullte","apache"} #set
+
+userInfo = {"name": "prateek", "age" : 18, "email" : "prateek@abc.com"} #dict (key value pair)
+
+switch = True #boolean 
+
+empty = None #Nonetype
+
+ghg = bytes([4,5,6]) # bytes object
+yhy = bytearray([1,2,3]) # bytearray object
+ada = frozenset([1, 2, 3]) # frozenset object
+
+
+# Identifiers rules
+# 1. Can contain letters (a-z, A-Z), digits (0-9), and underscores (_).
+# 2. Cannot start with a digit.
+# 3. Cannot contain special characters (e.g., @, #, $, %, etc.).
+# 4. Cannot be a reserved keyword (e.g., if, else, for, while, etc.).
+# 5. Case-sensitive (e.g., myVar and myvar are different identifiers).
+# 6. Should be descriptive and meaningful.
+
+
+# mutable and im-mutable
+# mutable: list, set, dictionary, bytearray
+# im-mutable: int, float, complex, string, tuple, frozenset, bytes, range, None, boolean
+
+
+
+#properties of list
+    #square brakets
+    #index based operation
+    #mutable 
+    #allowed duplicate values
+    #order colection
+
+a = [12,23,12,23,34,123,32414]
+a[0] #12
+a[3]
+
+#properties of tuple
+    #inmutable version of list
+    #round brackets/paranthesis
+    #index based operation
+    #ordered collection
+    #allowed duplicate values
+
+#properties of set
+    #curly brace
+    #duplicate values not allowed
+    #unordered colection
+    #mutable
+    #no indexing,no slicing
+    #homegenous and hetrogeneous 
+
+#properties of dict
+    #curly brackets with key-value pairs
+    #mutable
+    #ordered collection
+    #keys must be unique and immutable datatypes
+    #no indexing
+
