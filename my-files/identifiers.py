6906af5b91715a1f81bdaabcf21c322a3c7b6e29
Test Demo <prateekc53@gmail.com>
2025-08-16 21:43:17 +0530

    updated identifiers

---
+++ my-files/identifiers.py
@@ -0,0 +1,31 @@
+#Identifiers  - In Python, an identifier is the name 
+#you give to things like variables, functions, classes, modules, or objects.
+
+#Rules of identifiers
+
+#1. Can contain letters (A–Z, a–z), digits (0–9), and underscore (_).
+#Example: my_variable1, studentName, _temp.
+
+#2. Cannot start with a digit.
+#❌ 1variable (invalid)
+#✅ variable1 (valid)
+
+#3. Cannot conatin special characters.
+#❌ my-var, total$ (invalid)
+
+#4. Cannot be a reserved keyword
+#❌ if, while, class, def, etc. cannot be used as identifiers.
+
+#5. Case-Sensitive
+# age, Age, and AGE are three different identifiers.
+
+#6. No spaces allowed
+#student name (invalid)
+#✅ student_name (valid)
+
+#Examples of identitfiers -
+#name
+#_name
+#student1
+#total_marks
+#calculateSum
