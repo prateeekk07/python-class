a150b6b88e87d0ba8b9bf49e97521700ce444dc4
Test Demo <prateekc53@gmail.com>
2025-08-18 10:56:00 +0530

    updated list file

--- my-files/datatypes/list.py
+++ my-files/datatypes/list.py
@@ -6,3 +6,17 @@
 # allowed duplicate values
 # homegeneous and heterogeneous
 
+l1 = [23,"per",34,[54,45,88],{"name":"Prateek"}]
+
+print(l1) #[23, 'per', 34, [54, 45, 88], {'name': 'Prateek'}]
+
+print(l1[2]) #34
+
+print(l1[1:3]) #['per', 34] 3 is excluded
+
+print(l1[-4:-1]) #['per', 34, [54, 45, 88] -1 is excluded
+
+l1[0] = 2
+
+print(l1) #[2, 'per', 34, [54, 45, 88], {'name': 'Prateek'}]
+
