0be098f7943ddd07aea1fe99484c198bb15e06ea
Test Demo <prateekc53@gmail.com>
2025-08-23 22:39:35 +0530

    practice updates

--- Class-files/Assignment_1_Prateek_Chouhan_20_08_2025.py
+++ Class-files/Assignment_1_Prateek_Chouhan_20_08_2025.py
@@ -177,3 +177,258 @@ print(h1) #-10, -9, -8, -7, -6, -5, -4, -3, -2
 
 
 
+# if, elif, else
+
+#if
+
+# a = 21
+
+#example 1
+# if a==18:
+#    print("hello world") #hello world only if a is equal to 18
+
+#example 2
+# games = ["rdr2","forza horizon","GTA"]
+
+# if "rdr2" in games:
+#     print("yes the game is in list")
+
+#example 3
+# num = 0
+
+# if num != 1:
+#     print("it is 0")
+
+
+
+#if-else
+
+#example 1
+#if a <= 20:
+#    print("you are correct")
+#else:
+#    print("you are wrong") 
+
+#example - 2
+#age = int(input("please enter your age"))
+
+#if age >= 18:
+#    print("you can drive the bike")
+#else:
+#    print("you are noob")
+
+#example - 3
+
+# switch = True
+
+# if switch == True:
+#     print("switch is ON")
+# else:
+#     print("switch is OFF")
+
+#example - 4
+
+# Player_name = "Cristiano Roanldo"
+
+# user_input = input("Please enter player name")
+
+# if user_input.lower() == Player_name.lower():
+#     print(f"you are a {Player_name} Fan")
+# else:
+#     print(f"you are not a {Player_name} Fan")
+
+#example - 5
+
+# order = ["burger","cold coffee","french fires"]
+
+# if "burgerr" in order:
+#     print("order contains burger")
+# else:
+#     print("items missing in order")
+
+#if else if
+
+#example - 1
+
+#marks = int(input("please enter your marks"))
+
+#if marks >= 90:
+#    print("you are top 1%")
+#elif marks >= 80:
+#    print("you are top 10%")
+#elif marks >= 70:
+#    print("you are top 30%")
+#elif marks >= 60:
+#    print("you are top 50%")
+#else:
+#    print("you are fail")
+
+#example - 2
+
+# username = "prateek"
+# password = "p1234"
+
+# entered_user = input("Enter username: ")
+# entered_pass = input("Enter password: ")
+
+# if entered_user == username and entered_pass == password:
+#     print("Login successful!")
+# elif entered_user == username and entered_pass != password:
+#     print("Incorrect password.")
+# else:
+#     print("User not found.")
+
+#example - 3
+
+# temperature = 10
+
+# if temperature > 35:
+#     print("It's very hot")
+# elif temperature > 20:
+#     print("Nice weather")
+# elif temperature > 10:
+#     print("A bit cold")
+# else:
+#     print("Freezing")
+
+
+#example - 4
+
+# choice = input("Choose your coffee (espresso/latte/cappuccino): ").lower()
+
+# if choice == "espresso":
+#     print("Here’s your strong espresso")
+# elif choice == "latte":
+#     print("Here’s your creamy latte")
+# elif choice == "cappuccino":
+#     print("Here’s your frothy cappuccino")
+# else:
+#     print("Sorry, we don’t serve that drink")
+
+# for and while loop
+
+# for with all iterables
+
+#list
+
+# l1 = ["das",5415,"ttt",989,122,"aawd"]
+
+# for i in l1:
+#     print(i, end=" ") #das 5415 ttt 989 122 aawd 
+
+#tuple 
+
+# t1 = (2143,"rrt","dasd",98764,(88,99),"gg")
+
+# for i in t1:
+#     print(i , end="|") #2143|rrt|dasd|98764|(88, 99)|gg|
+
+#range 
+
+# for i in range(100):
+#     print(i,end="-") #0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25-26-27-28-29-30-31-32-33-34-35-36-37-38-39-40-41-42-43-44-45-46-47-48-49-50-51-52-53-54-55-56-57-58-59-60-61-62-63-64-65-66-67-68-69-70-71-72-73-74-75-76-77-78-79-80-81-82-83-84-85-86-87-88-89-90-91-92-93-94-95-96-97-98-99-
+
+#string
+
+# marvel = "i am batman"
+
+# for i in marvel:
+#     print(i, end="") #i am batman
+
+
+#for loop with if statement
+
+#example 1
+# l1 = [11,22,33,44,55,66,7,88,99]
+
+# for i in l1:
+#     if i == 7:
+#         print(f"breaking the loop for {i}")
+#     else:
+#         print(i)
+# 11
+# 22
+# 33
+# 44
+# 55
+# 66
+# breaking the loop for 7
+# 88
+# 99
+
+#example 2
+# t1 = (321,3425,132,"ads",6521,34,"ttt")
+
+# for i in t1:
+#     if type(i) == str:
+#         print(f"you are printing a string {i}")
+#     else:
+#         print(i)
+
+#example 3
+# games = ["gta","batman","fifa"]
+# user_email = input("Please enter your stored game").lower()
+
+
+# for i in games:
+#     if i == "gta" and i == user_email:
+#         print("you are gta game")
+#     elif i == "batman" and i == user_email:
+#         print("you are into batman game")
+#     elif i == "fifa" and i == user_email:
+#         print("you are into fifa game")
+
+#while loop with ifinite
+
+# while True:
+#     print("This will run forever!")
+
+#while loop wit counter
+
+# c = 1
+
+# while c <= 10:
+#     print(c)
+#     c += 1
+
+
+#for loop with pass, continue, break
+
+# for i in range(10):
+#     if i == 5:
+#         pass #will do nothing
+#     print(i)
+
+# for i in range(10):
+#     if i == 5:
+#         continue #will skip it 
+#     print(i)
+
+
+# for i in range(100):
+#     if i == 50:
+#         break #will break the loop
+#     print(i)
+
+#while loop with pass, continue, break
+
+# i = 0
+# while i < 20:
+#     if i == 16:
+#         pass  # placeholder, loop just continues normally
+#     print("Number:", i)
+#     i += 1
+
+# i = 0
+# while i < 5:
+#     i += 1
+#     if i == 2:
+#         continue  # skip printing when i = 2
+#     print("Number:", i)
+
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         break  # exit loop when i = 3
+#     print("Number:", i)
+#     i += 1
--- my-files/Control Flow/tempCodeRunnerFile.py
+++ my-files/Control Flow/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-Cristiano Roanldo
+temperature = 10
+
+if temperature > 35:
+    print("It's very hot")
+elif temperature > 20:
+    print("Nice weather")
+elif temperature > 10:
+    print("A bit cold")
+else:
+    print("Freezing")
