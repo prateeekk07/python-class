75e2ae083b12d8da6b91ae5dd42562c0107d06d7
Test Demo <prateekc53@gmail.com>
2025-08-23 18:35:14 +0530

    practice updates

--- my-files/Control Flow/conditional-statements.py
+++ my-files/Control Flow/conditional-statements.py
@@ -1,21 +1,75 @@
 # if, elif, else
 
-#a = 21
+#if
 
-#if a==18:
+# a = 21
+
+#example 1
+# if a==18:
 #    print("hello world") #hello world only if a is equal to 18
 
+#example 2
+# games = ["rdr2","forza horizon","GTA"]
+
+# if "rdr2" in games:
+#     print("yes the game is in list")
+
+#example 3
+# num = 0
+
+# if num != 1:
+#     print("it is 0")
+
+
+
+#if-else
+
+#example 1
 #if a <= 20:
 #    print("you are correct")
 #else:
 #    print("you are wrong") 
 
+#example - 2
 #age = int(input("please enter your age"))
 
 #if age >= 18:
 #    print("you can drive the bike")
 #else:
 #    print("you are noob")
+
+#example - 3
+
+# switch = True
+
+# if switch == True:
+#     print("switch is ON")
+# else:
+#     print("switch is OFF")
+
+#example - 4
+
+# Player_name = "Cristiano Roanldo"
+
+# user_input = input("Please enter player name")
+
+# if user_input.lower() == Player_name.lower():
+#     print(f"you are a {Player_name} Fan")
+# else:
+#     print(f"you are not a {Player_name} Fan")
+
+#example - 5
+
+# order = ["burger","cold coffee","french fires"]
+
+# if "burgerr" in order:
+#     print("order contains burger")
+# else:
+#     print("items missing in order")
+
+#if else if
+
+#example - 1
 
 #marks = int(input("please enter your marks"))
 
@@ -30,6 +84,8 @@
 #else:
 #    print("you are fail")
 
+#example - 2
+
 # username = "prateek"
 # password = "p1234"
 
@@ -42,3 +98,31 @@
 #     print("Incorrect password.")
 # else:
 #     print("User not found.")
+
+#example - 3
+
+# temperature = 10
+
+# if temperature > 35:
+#     print("It's very hot")
+# elif temperature > 20:
+#     print("Nice weather")
+# elif temperature > 10:
+#     print("A bit cold")
+# else:
+#     print("Freezing")
+
+
+#example - 4
+
+# choice = input("Choose your coffee (espresso/latte/cappuccino): ").lower()
+
+# if choice == "espresso":
+#     print("Here’s your strong espresso")
+# elif choice == "latte":
+#     print("Here’s your creamy latte")
+# elif choice == "cappuccino":
+#     print("Here’s your frothy cappuccino")
+# else:
+#     print("Sorry, we don’t serve that drink")
+
