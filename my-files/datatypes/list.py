839e3aa0bfcca95182eb85a9071b3e64f55dc1fe
Test Demo <prateekc53@gmail.com>
2025-09-01 20:22:56 +0530

    practice

--- my-files/datatypes/list.py
+++ my-files/datatypes/list.py
@@ -6,24 +6,37 @@
 # allowed duplicate values
 # homegeneous and heterogeneous
 
-l1 = [23,"per",34,[54,45,88],{"name":"Prateek"}]
+# l1 = [23,"per",34,[54,45,88],{"name":"Prateek"}]
 
-print(l1) #[23, 'per', 34, [54, 45, 88], {'name': 'Prateek'}]
+# print(l1) #[23, 'per', 34, [54, 45, 88], {'name': 'Prateek'}]
 
-print(l1[2]) #34
+# print(l1[2]) #34
 
-print(l1[1:3]) #['per', 34] 3 is excluded
+# print(l1[1:3]) #['per', 34] 3 is excluded
 
-print(l1[-4:-1]) #['per', 34, [54, 45, 88] -1 is excluded
+# print(l1[-4:-1]) #['per', 34, [54, 45, 88] -1 is excluded
 
-l1[0] = 2
+# l1[0] = 2
 
-print(l1) #[2, 'per', 34, [54, 45, 88], {'name': 'Prateek'}]
+# print(l1) #[2, 'per', 34, [54, 45, 88], {'name': 'Prateek'}]
 
-fruits = ["apple","mango","apple","graphes"]
-print(fruits)
+# fruits = ["apple","mango","apple","graphes"]
+# print(fruits)
 
-datatypes  =[True,"string",23,(51,24,51),{"ads","das","fff"}]
+# datatypes  =[True,"string",23,(51,24,51),{"ads","das","fff"}]
 
-print(datatypes)
+# print(datatypes)
+
+# list = []
+# print(dir(list))
+
+#'append','insert, 'clear', 'copy', 'count', 'extend', 'index', ', 'pop', 'remove', 'reverse', 'sort']
+
+#'append'
+
+list = [44,45,22,66]
+
+a = list.insert([1](77))
+
+print(list)
 
