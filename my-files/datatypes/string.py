2caa380da70943ba5f24637fa2eab930c848d4fd
Test Demo <prateekc53@gmail.com>
2025-09-06 21:39:05 +0530

    string practice

--- my-files/datatypes/string.py
+++ my-files/datatypes/string.py
@@ -3,13 +3,48 @@
 # immutable
 # index based and Sliceable
 
-fullName = "Prateek"
-print(fullName) #Prateek
+# fullName = "Prateek"
+# print(fullName) #Prateek
  
-username = "xyz123"
-u = username[3]
-print(u) #1
+# username = "xyz123"
+# u = username[3]
+# print(u) #1
 
-age = "20"
-print(age)
+# age = "20"
+# print(age)
+
+# fullname = "Prateek"
+
+# print(dir(fullname)) #to see all methods 
+
+# lower(), upper(), title(), capitalize(),
+# strip(), lstrip(), rstrip(),
+# find(), index(), startswith(), endswith(),count()
+# split(), join(), replace()
+
+# fullname = "Prateek raj singh chouhan"
+
+# print(fullname.upper()) #PRATEEK RAJ SINGH CHOUHAN
+# print(fullname.lower()) #prateek raj singh chouhan
+# print(fullname.title()) #Prateek Raj Singh Chouhan
+# print(fullname.capitalize()) #Prateek raj singh chouhan
+
+# print(fullname.strip) #removes white spaces from left and right
+# print(fullname.rstrip()) #removes white spaces from right
+# print(fullname.lstrip()) #removes white spaces from left
+
+# print(fullname.startswith("Prateek")) #True
+# print(fullname.endswith("cho")) #false 
+
+# print(fullname.find("a")) #2 will return -1 if not find the value
+
+# print(fullname.index("a")) #2 will throw an error if not find the value
+
+# print(fullname.count("a")) #3 
+
+fullname = "Prateek raj singh chouhan"
+
+print(" ".join(fullname))
+
+
 
