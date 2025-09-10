dd25d010f556c0467ee3346babbddf85222fdbb7
Test Demo <prateekc53@gmail.com>
2025-09-10 23:04:16 +0530

    list practice

--- my-files/datatypes/list.py
+++ my-files/datatypes/list.py
@@ -33,23 +33,37 @@
 #'append','insert, 'clear', 'copy', 'count', 'extend', 'index', ', 'pop', 'remove', 'reverse', 'sort']
 
 # Add → append, extend, insert
-
-l1 = [34,55,77,44,322,33,88]
-l2 = [5,6,9,0]
+# l1 = [34,55,77,44,322,33,88]
+# l2 = [5,6,9,0]
 
 # l1.append(99)
 # print(l1)
 # l1.extend(l2)
 # print(l1)
-
-l1.insert(6,"abhijeet")
-print(l1)
+# l1.insert(6,"abhijeet")
+# print(l1)
 
 # Remove → pop, remove, clear
+
+# out=l1.pop(3)  we can do it with index value #stores 44 the value as well
+# print(l1) #[34, 55, 77, 322, 33, 88]
+
+# l1.remove(44) #we can do it with index value #no value stored in this
+# print(l1) #[34, 55, 77, 322, 33, 88]
+
+# l1.clear()
+# print(l1) #[]
 
 # Info → index, count
 
 # Order → reverse, sort
+l1 = [34,55,77,44,322,33,88]
+l2 = [5,6,9,0]
+
+l1.sort()
+l1.sort(reverse=True) 
+print(l1)
+
 
 # Copy → copy
 
