78cd101098595cbe2f75639e8885874b26b56fa9
prateeekk07 <prateekc53@gmail.com>
2025-09-28 15:47:06 -0500

    added multiple files on 28th sept

--- my-files/excel_file_handling.py
+++ my-files/excel_file_handling.py
@@ -1,16 +1,24 @@
 import openpyxl
 
-wb = openpyxl.Workbook()
-sheet = wb.active
+# wb = openpyxl.Workbook()
+# sheet = wb.active
 
 #1st Way
 
-sheet["A1"] = "Hello"
-sheet["B1"] = "World!"
-sheet["D5"] = "dUMMY data"
+# sheet["A1"] = "Hello"
+# sheet["B1"] = "World!"
+# sheet["D5"] = "dUMMY data"
 
-#2nd Way
+# #2nd Way
 
-sheet.cell(row=10, column=6).value = "Python"
+# sheet.cell(row=10, column=6).value = "Python"
 
-wb.save(filename=r"D:\python b14\python-class\my-files\test.xlsx")
+# for i in range(1,11):
+#     sheet.cell(row=i, column=i).value = i
+
+FILE_PATH = r"D:\python b14\python-class\my-files\test.xlsx"
+
+wb = openpyxl.load_workbook(FILE_PATH)
+print(wb.sheetnames)
+
+# wb.save(filename=r"D:\python b14\python-class\my-files\test.xlsx")
