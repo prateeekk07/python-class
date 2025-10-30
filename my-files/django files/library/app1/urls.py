eef933d7b0b4fbf16251b3b771e7271153c1ea3c
Test Demo <prateekc53@gmail.com>
2025-10-30 23:21:55 +0530

    added djano project library on windows

--- my-files/django files/library/app1/urls.py
+++ my-files/django files/library/app1/urls.py
@@ -4,5 +4,7 @@ from . import views # import your views fromthe same app
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('book-list', views.show_books, name='show_books'),
+    path('book-list/', views.show_books, name='show_books'),
+    path('delete-book/<int:pk>', views.delete_book, name='delete_book'),
+    path('update-book/<int:pk>', views.update_book, name='update_book'),
 ]
--- my-files/django files/library/app1/views.py
+++ my-files/django files/library/app1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render ,  HttpResponse
+from django.shortcuts import render , HttpResponse, redirect
 from app1.models import Book
 
 # Create your views here.
@@ -6,12 +6,24 @@ from app1.models import Book
 def home(request):
     if request.method == "POST":
         data = request.POST
+        book_id = data.get("id")
+
         book_name = data.get("nm")
         book_qty = data.get("qty")
         book_price = data.get("prc")
         book_author = data.get("auth")
-        Book.objects.create(name=book_name, qty=book_qty, price=book_price, author=book_author)
-        return HttpResponse("Book added successfully!!")
+        if not book_id:
+            Book.objects.create(name=book_name, qty=book_qty, price=book_price, author=book_author)
+        else:
+            book_obj = Book.objects.get(id=book_id)
+            book_obj.name = book_name
+            book_obj.qty = book_qty
+            book_obj.price = book_price
+            book_obj.author = book_author
+            book_obj.save()
+    
+
+        return redirect("show_books")
     else:
         return render(request, "home.html")
 
@@ -19,8 +31,11 @@ def show_books(request):
     books = Book.objects.all()
     return render(request, "show_books.html", {"all_books": books})
 
-def update_book(request):
-    pass
+def update_book(request,pk):
+    return render(request,"home.html",{"single_book": Book.objects.get(id=pk)})
+    
 
-def delete_book(request):
-    pass
+def delete_book(request,pk):
+    book = Book.objects.get(id=pk)
+    book.delete()
+    return redirect("show_books")
